dd7b4ef89917da563b7f96e926b4de2e00cd1b24
kgobakis <kostas004@hotmail.com>
2021-03-09 22:15:15 +0200

    made stars dynamic based on rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,25 @@
 import React from "react";
 import data from "./data";
-
+import Product from "./components/Product";
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">
+          <a className="brand" href="/">
             Beatbox Shop
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product.id} className="card">
-                <a href={`/product/${product.id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product.id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"> </i>{" "}
-                    </span>
-                    <span>
-                      <i className="fa fa-star"> </i>{" "}
-                    </span>
-                    <span>
-                      <i className="fa fa-star"> </i>{" "}
-                    </span>
-                    <span>
-                      <i className="fa fa-star"> </i>{" "}
-                    </span>
-                    <span>
-                      <i className="fa fa-star"> </i>{" "}
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
+              <Product key={product.id} product={product} />
             ))}
           </div>
         </div>
