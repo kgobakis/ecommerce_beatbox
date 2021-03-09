0f999c18f7c00ab70f23070dc6de615bcdfe1d33
kgobakis <kostas004@hotmail.com>
2021-03-09 22:53:27 +0200

    replaced arrow fn with regular expressed ones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,30 @@
 import React from "react";
-import data from "./data";
-import Product from "./components/Product";
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Beatbox Shop
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product.id} product={product} />
-            ))}
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Beatbox Shop
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className="row center">All rights reserved.</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All rights reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
