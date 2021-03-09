412b194351b47f56b539a8721d11911e7f5c5153
kgobakis <kostas004@hotmail.com>
2021-03-09 22:32:51 +0200

    added spacing between stars and reviews

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -5,67 +5,68 @@ export default function Rating(props) {
 
   return (
     <div className="rating">
-      {rating < 0.5 ? <p>No reviews yet.</p> : ""}
-      <span>
-        <i
-          className={
-            rating >= 1
-              ? "fa fa-star"
-              : rating >= 0.5
-              ? "fa fa-star-half-alt"
-              : "far fa-star"
-          }
-        >
-          {" "}
-        </i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 2
-              ? "fa fa-star"
-              : rating >= 1.5
-              ? "fa fa-star-half-alt"
-              : "far fa-star"
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 3
-              ? "fa fa-star"
-              : rating >= 2.5
-              ? "fa fa-star-half-alt"
-              : "far fa-star"
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 4
-              ? "fa fa-star"
-              : rating >= 3.5
-              ? "fa fa-star-half-alt"
-              : "far fa-star"
-          }
-        >
-          {" "}
-        </i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 5
-              ? "fa fa-star"
-              : rating >= 4.5
-              ? "fa fa-star-half-alt"
-              : "far fa-star"
-          }
-        ></i>
-      </span>
-      <span>{numReviews + " reviews"}</span>
+      <div>
+        <span>
+          <i
+            className={
+              rating >= 1
+                ? "fa fa-star"
+                : rating >= 0.5
+                ? "fa fa-star-half-alt"
+                : "far fa-star"
+            }
+          >
+            {" "}
+          </i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 2
+                ? "fa fa-star"
+                : rating >= 1.5
+                ? "fa fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 3
+                ? "fa fa-star"
+                : rating >= 2.5
+                ? "fa fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 4
+                ? "fa fa-star"
+                : rating >= 3.5
+                ? "fa fa-star-half-alt"
+                : "far fa-star"
+            }
+          >
+            {" "}
+          </i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 5
+                ? "fa fa-star"
+                : rating >= 4.5
+                ? "fa fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+      </div>
+      <span className="reviews">{numReviews + " reviews"}</span>
     </div>
   );
 }
