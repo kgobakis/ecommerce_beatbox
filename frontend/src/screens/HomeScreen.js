28c8e3ee8d18006bb5b077762187d39d55de2f60
kgobakis <kostas004@hotmail.com>
2021-03-11 00:19:15 +0200

    added backend; and connected it to frontend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
-import data from "../data";
+import axios from "axios";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className="row center">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <Product key={product.id} product={product} />
         ))}
       </div>
