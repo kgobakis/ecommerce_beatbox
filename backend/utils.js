b5ad7e7a1ef3eca02d1e4a099491da07a1045db8
kgobakis <kostas004@hotmail.com>
2021-03-15 21:00:45 +0200

    added middleware to authenticate order requests
    created model to save order info and items
    
    created api for orders

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,5 @@
-import jwt from "jsonwebtoken";
+import e from "express";
+import jwt, { decode } from "jsonwebtoken";
 
 export const generateToken = (user) => {
   return jwt.sign(
@@ -13,4 +14,28 @@ export const generateToken = (user) => {
       expiresIn: "30d",
     }
   );
+};
+
+/* Middleware to authenticate user*/
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer: Token...(first 7 chars are the Bearer)
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "somerandomsecret",
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: "Invalid Token" });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: "There is not Token" });
+  }
 };
